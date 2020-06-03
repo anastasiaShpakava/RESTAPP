825be387247c410ca10a866166a71612e22a5232
Anastasia Shpakova <shpakavaa@gmail.com>
2020-06-03 23:21:56 +0300

    SecurityConfig

---
+++ src/main/java/com/leverX/blog/controller/UserController.java
@@ -0,0 +1,41 @@
+//package com.leverX.blog.controller;
+//
+//import com.leverX.blog.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.ApplicationEventPublisher;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class UserController {
+//    private final UserService userService;
+//    private final ApplicationEventPublisher applicationEventPublisher;
+//
+//    @PostMapping("/save")
+//    public UserDto saveUsers(@RequestBody UserDto userDto) throws ValidationException {
+//        log.info("Handling save users: " + userDto);
+//        return userService.saveUser(userDto);
+//    }
+//
+//    @GetMapping("/findAll")
+//    public List<UserDto> findAllUsers() {
+//        log.info("Handling find all users request");
+//        return userService.findAll();
+//    }
+//
+//    @GetMapping("/findByFirstName")
+//    public UserDto findByFirstName(@RequestParam String firstName) {
+//        log.info("Handling find by first name request: " + firstName);
+//        return userService.findByFirstName(firstName);
+//    }
+//
+//    @DeleteMapping("/delete/{id}")
+//    public ResponseEntity<Void> deleteUsers(@PathVariable Integer id) {
+//        log.info("Handling delete user request: " + id);
+//        userService.deleteUser(id);
+//        return ResponseEntity.ok().build();
+//    }
+//}
